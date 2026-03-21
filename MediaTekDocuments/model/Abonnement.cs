187ae781a7ee3856700a7f1253705fe4a727cb8a
webby! <sritarbrt@gmail.com>
2026-03-21 18:49:02 +0100

    feat: Ajout de la classe abonnement

---
+++ MediaTekDocuments/model/Abonnement.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+namespace MediaTekDocuments.model
+{
+    /// <summary>
+    /// Classe métier Abonnement représentant un abonnement à une revue
+    /// </summary>
+    public class Abonnement
+    {
+        public string Id { get; }
+        public DateTime DateFinAbonnement { get; }
+        public string IdRevue { get; }
+        public DateTime DateCommande { get; }
+        public double Montant { get; }
+
+        public Abonnement(string id, DateTime dateFinAbonnement, string idRevue, DateTime dateCommande, double montant)
+        {
+            this.Id = id;
+            this.DateFinAbonnement = dateFinAbonnement;
+            this.IdRevue = idRevue;
+            this.DateCommande = dateCommande;
+            this.Montant = montant;
+        }
+    }
+}
+
