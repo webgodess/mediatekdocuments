948f32f263c28865bc739b12a30d1498916ea807
webby! <sritarbrt@gmail.com>
2026-03-21 18:50:33 +0100

    feat: Ajout de la classe Suivi

---
+++ MediaTekDocuments/model/Suivi.cs
@@ -0,0 +1,12 @@
+﻿namespace MediaTekDocuments.model
+{
+    /// <summary>
+    /// Classe métier Suivi  
+    /// </summary>
+    public class Suivi : Categorie
+    {
+        public Suivi(string id, string libelle) : base(id, libelle)
+        {
+        }
+    }
+}
